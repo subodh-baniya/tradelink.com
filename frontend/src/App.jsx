a49ffdb7bea2284262b6865566812789aae1c1b7
subodh-baniya <baniyasubodh07@gmail.com>
2025-12-04 13:40:03 +0545

    added the layout of login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
-import react from "react"
+
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
 import Login from "./pages/Login"
 import Register from "./pages/Register"
 import Home from "./pages/Home"
 import NotFound from "./pages/NotFound"
 import ProtectedRoute from "./components/ProtectedRoute"
+
 
 function Logout() {
   localStorage.clear()
