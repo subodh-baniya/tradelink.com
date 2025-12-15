f1468524c71e344f062262275ca73b8b5edcbbdc
subodh-baniya <baniyasubodh07@gmail.com>
2025-12-15 18:40:21 +0545

    made some minor deletion

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+
 import { HiMenu } from "react-icons/hi";
 import { IoNotificationsSharp } from "react-icons/io5";
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
+  const navigate = useNavigate(); 
+
+  const Logout = () => {
+    localStorage.clear();
+    navigate("/login"); 
+  };
+
   return (
     <div className='flex w-screen h-16 p-3 shadow-xl rounded justify-between items-center '>
      <div>
@@ -11,8 +19,7 @@ const Header = () => {
 
      <div className='flex gap-3'>
        <IoNotificationsSharp className="text-2xl mr-4 cursor-pointer"/>
-       <HiMenu className="text-2xl mr-4 cursor-pointer" />
-        <p></p>
+       <HiMenu className="text-2xl mr-4 cursor-pointer" onClick={Logout} />
      </div>
     </div>
   )
