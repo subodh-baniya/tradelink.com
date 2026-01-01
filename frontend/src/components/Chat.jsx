6edf391977a8ee0f1991b89d77accb8b4e176635
subodh-baniya <baniyasubodh07@gmail.com>
2026-01-01 18:18:20 +0545

    made some css changes

--- frontend/src/components/Chat.jsx
+++ frontend/src/components/Chat.jsx
@@ -1,18 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const Chat = () => {
   return (
-    <div className='grid grid-cols-6'>
-      <div className='col-span-2'>
-          <p>chats</p>
-      </div>
-
-      <div className='col-span-4'>
-          <p>conversation</p>
-      </div>
+    <div>
       
     </div>
-  )
-}
+  );
+};
 
-export default Chat
+export default Chat;
